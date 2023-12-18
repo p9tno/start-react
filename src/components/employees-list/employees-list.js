dbfb2a30206b7aaaaa6b990799396a1a86612808
p9tno <plahotin_s@mail.ru>
2023-12-19 00:21:30 +0300

    создали структуры приложения

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,15 @@
+import './employees-list.css';
+
+import EmployeesListItem from '../employees-list-item/employees-list-item';
+
+function EmployeesList() {
+    return (
+        <ul className="app-list list-group">
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+        </ul>
+    )
+}
+
+export default EmployeesList;
