1281572c9ed1185d9050749494601efcc448750f
p9tno <plahotin_s@mail.ru>
2024-01-17 20:58:24 +0300

    начал разбиратться с состояниями через классы

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,16 +3,19 @@ import './employees-list.css';
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 function EmployeesList({data}) {
-    // console.log(d ata);
+    // console.log(data);
     const elements = data.map( item => {
+        // вариант 1
         return <EmployeesListItem 
                 key={item.id}
                 name={item.name} 
                 salary={item.salary}
                 increase={item.increase}/>
 
-        return <EmployeesListItem {...item}/>
-
+        // вариант 2  если не нужен key
+        // return <EmployeesListItem {...item}/>
+        
+        // вариант 3
         // const {id, ...itemProps} = item;
         // return <EmployeesListItem key={id} {...itemProps}/>
     })
